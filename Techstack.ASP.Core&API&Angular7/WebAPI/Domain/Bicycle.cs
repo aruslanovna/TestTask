154a29052e43cb82169f0233b9b7cf2d0c1bd0c2
Анастасія Христова <anastasiia.khrystova@nure.ua>
2020-07-02 12:33:08 +0300

    fix domain

--- Techstack.ASP.Core&API&Angular7/WebAPI/Domain/Bicycle.cs
+++ Techstack.ASP.Core&API&Angular7/WebAPI/Domain/Bicycle.cs
@@ -4,7 +4,7 @@ namespace Domain
 {
     public class Bicycle
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public Type Type { get; set; }
         public int Price { get; set; }
